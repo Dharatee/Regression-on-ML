cbccb1a1cc49d25ca71de721b68dd9008928b5a1
Angrybird <shrestha_dss@hotmail.com>
2019-05-13 18:10:45 +0545

    features

--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 #get data from quandl.com
 df=quandl.get("FINRA/FNRA_GOOGL", authtoken="QXLRYqyjrDN-DaW3ui8i", collapse="monthly", start_date="2016-04-03")
@@ -10,5 +11,14 @@ print(df.head())
 df=df[['ShortVolume','TotalVolume']]
 df['DIF_PCT']=(df['TotalVolume']-df['ShortVolume'])/df['TotalVolume']*100.0
 
-df=df[['ShortVolume','TotalVolume','DIF_PCT']]
+df=df[['ShortVolume','TotalVolume','DIF_PCT']] #these are features
+#print(df.head())
+
+forecastCol='ShortVolume'
+df.fillna(-99999, inplace=True)
+
+forecastOut=int(math.ceil(0.1*len(df)))
+
+df['label']=df[forecastCol].shift(-forecastOut)
+
 print(df.head())
